84c59cb891a4e27f00e3a6678543704dd6f7c024
Dar1ux <djankaus87@gmail.com>
2023-05-29 17:15:49 +0300

    cleared unnecessary lines

--- Final Assignment/js/main.js
+++ Final Assignment/js/main.js
@@ -4,11 +4,5 @@
     var r = 255 - x;
     var g = 192 - x;
     var b = 203 - x;
-    //h1[0].innerHTML = "rgb(" + r + "," + g + "," + b + ")";
-    //h1[0].innerHTML = 'rgb(' + toString(r) + ',' + toString(g) + ',' + toString(b);
     h1[0].style.color = "rgb(" + r + "," + g + "," + b + ")";
-    
-    
 }
-setInterval(changeColor('red'), 2000);
-setInterval(changeColor('blue'),2000);
